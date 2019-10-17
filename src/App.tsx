495f5508a340f816494163d1e5e0ee51c046d33a
Anisha Jain <anishajain@outlook.com>
2019-10-17 02:30:15 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        HELLO WORLD
+      </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
