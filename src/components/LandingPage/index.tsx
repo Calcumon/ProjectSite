813b5b601db40fdf80084b86307446933a680f62
Anisha Jain <anishajain@outlook.com>
2019-10-18 09:35:37 -0700

    features added

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,50 +1,69 @@
 import React from 'react';
-import Container from '../Container'
-import './styles.scss'
-import cutie from '../../assets/cuties/cutie3.png'
-
-const LandingPageFeatureData: LandingPageFeatureProps[] = [
-    {
-        title: "Kids love it.",
-        description: "They steer their car into the right answer while speed racing in our app!",
-        image: "",
-    },
-    {
-        title: "Play on the GO",
-        description: "On a long car ride? Download our mobile app to play on the GO!",
-        image: "",
-    },
-    {
-        title: "Together, we learn",
-        description: "Kids can compete with their friends with our multiplayer option! (Coming soon)",
-        image: "",
-    },
-    {
-        title: "Shopping? Ooh.",
-        description: "Kids can customize their characters using game currency! (Coming Soon)",
-        image: "",
-    }
-]
+import Container from '../Container';
+import './styles.scss';
+import cutie from '../../assets/cuties/cutie3.png';
+import playIcon from '../../assets/features/play.png';
+import friendsIcon from '../../assets/features/play-with-friends.png';
+import shopingIcon from '../../assets/features/shop.png';
 
 interface LandingPageFeatureProps {
     title: string;
     description: string;
     image: string;
+    i: number;
 }
 
-const LandingPageFeature : React.FC<LandingPageFeatureProps> = ({ title, description, image }) => {
-    return (
-        <div>
-            <div>
+const LandingPageFeature : React.FC<LandingPageFeatureProps> = ({ title, description, image, i }) => {
+    if (i%2 === 0) {
+        return <div className="feature bluebg">
+            <div className="featureText">
                 <h2>{ title }</h2>
                 <p>{ description }</p>
             </div>
             <img src={ image } alt={ title }/>
         </div>
+    }
+    
+    return (
+        <div className="feature orangebg">
+            <img src={ image } alt={ title }/>
+            <div className="featureText">
+                <h2>{ title }</h2>
+                <p>{ description }</p>
+            </div>
+        </div>
     )
 }
 
 const LandingPage : React.FC = () => {
+
+    const LandingPageFeatureData: LandingPageFeatureProps[] = [
+        {
+            title: "Kids love it.",
+            description: "They steer their car into the right answer while speed racing in our app!",
+            image: "",
+            i: 0,
+        },
+        {
+            title: "Play on the GO",
+            description: "On a long car ride? Download our mobile app to play on the GO!",
+            image: playIcon,
+            i: 1,
+        },
+        {
+            title: "Together, we learn",
+            description: "Kids can compete with their friends with our multiplayer option! (Coming soon)",
+            image: friendsIcon,
+            i: 2,
+        },
+        {
+            title: "Shopping? Ooh.",
+            description: "Kids can customize their characters using game currency! (Coming Soon)",
+            image: shopingIcon,
+            i: 3,
+        }
+    ]
+
     return (
         <Container bgColor={ "greenbg" } currentPage="landing">
             <div className="banner">
@@ -55,6 +74,11 @@ const LandingPage : React.FC = () => {
                     <button>TRY THE APP > </button>
                 </div>
             </div>
+            { LandingPageFeatureData.map(({ title, description, image, i }) => <LandingPageFeature title={ title } 
+                                                                                                   description={ description } 
+                                                                                                   image={ image }
+                                                                                                   i={ i } />
+                                            ) }
         </Container>
     )
 }
