189725db35c1d0689f3ee461c74bda3b9cae7c2b
Anisha Jain <anishajain@outlook.com>
2019-10-18 20:57:29 -0700

    footer added

--- src/components/Container/components/Footer/index.tsx
+++ src/components/Container/components/Footer/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import './styles.scss';
 // TODO: What will be in our LoginInterface?
 interface FooterInterface {
 }
 
 const Footer : React.FC<FooterInterface> = () => {
     return (
-        <div>
-            <p> Footer </p>
+        <div className="footer-container">
+            <h2>WE'RE LAUNCHING SOON!</h2>
+            <div>
+                <input placeholder="Enter your email"></input>
+                <button>JOIN THE WAITLIST</button>
+            </div>
         </div>
     )
 }
