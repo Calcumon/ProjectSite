1981ec7894e0e88ce71f9e551e813fc6985c3933
Anisha Jain <anishajain@outlook.com>
2019-11-19 22:29:18 -0800

    images added

---
+++ client/src/components/AboutUs/components/Team/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './styles.scss';
+import anisha from '../../../../assets/team/anisha.png';
+import joey from '../../../../assets/team/joey.png';
+import lucia from '../../../../assets/team/lucia.png';
+import anna from '../../../../assets/team/anna.png';
+import erica from '../../../../assets/team/erica.png';
+import drew from '../../../../assets/team/drew.png';
+import kj from '../../../../assets/team/kj.png';
+
+const Team : React.FC = () => {
+    return (
+        <div className="team">
+            <h1>WHO WE ARE</h1>
+            <div className="team-cards">
+                <div className="col1">
+                    <div>
+                        <img src={anisha} alt="Anisha's picture" />
+                    </div>
+                    <div >
+                        <img src={joey} alt="Joey's picture" />
+                    </div>
+                    <div>
+                        <img src={lucia} alt="Lucia's picture" />
+                    </div>
+                </div>
+                <div className="col2">
+                    <div>
+                        <img src={anna} alt="Anna's picture" />
+                    </div>
+                    <div>
+                        <img src={erica} alt="Erica's picture" />
+                    </div>
+                </div>
+                <div className="col3">
+                    <div>
+                        <img src={drew} alt="Drew's picture" />
+                    </div>
+                    <div>
+                        <img src={kj} alt="Kjs picture" />
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Team;
--- client/src/components/AboutUs/index.tsx
+++ client/src/components/AboutUs/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Container from '../Container';
 import Banner from './components/Banner';
 import Story from './components/Story';
+import Team from './components/Team';
 
 const AboutUs : React.FC = () => {
     return (
         <Container currentPage="aboutus" bgColor="whitebg">
             <Banner />
             <Story />
+            <Team />
         </Container>
     )
 }
