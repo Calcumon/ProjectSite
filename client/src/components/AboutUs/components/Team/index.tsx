e5360c618933417abaabebd97d5e4bb1f0021985
Anisha Jain <anishajain@outlook.com>
2019-11-20 09:52:06 -0800

    text for everyone added

--- client/src/components/AboutUs/components/Team/index.tsx
+++ client/src/components/AboutUs/components/Team/index.tsx
@@ -23,25 +23,55 @@ const Team : React.FC = () => {
                         <img src={anisha} alt="Anisha's picture" />
                     </div>
                     <div >
+                        <div className="info">
+                            <p className="h3">Joey Gaitan</p>
+                            <p className="h4">Frontend</p>
+                            <p>icons</p>
+                        </div>
                         <img src={joey} alt="Joey's picture" />
                     </div>
                     <div>
+                        <div className="info">
+                            <p className="h3">Lucia Reynoso</p>
+                            <p className="h4">Git master</p>
+                            <p>icons</p>
+                        </div>
                         <img src={lucia} alt="Lucia's picture" />
                     </div>
                 </div>
                 <div className="col2">
                     <div>
+                        <div className="info">
+                            <p className="h3">Anna Pawl</p>
+                            <p className="h4">Design Lead, UX</p>
+                            <p>icons</p>
+                        </div>
                         <img src={anna} alt="Anna's picture" />
                     </div>
                     <div>
+                        <div className="info">
+                            <p className="h3">Erica Naglik</p>
+                            <p className="h4">Design</p>
+                            <p>icons</p>
+                        </div>
                         <img src={erica} alt="Erica's picture" />
                     </div>
                 </div>
                 <div className="col3">
                     <div>
+                        <div className="info">
+                            <p className="h3">Drew McGowan</p>
+                            <p className="h4">Backend</p>
+                            <p>icons</p>
+                        </div>
                         <img src={drew} alt="Drew's picture" />
                     </div>
                     <div>
+                        <div className="info">
+                            <p className="h3">KJ Wilson</p>
+                            <p className="h4">Backend</p>
+                            <p>icons</p>
+                        </div>
                         <img src={kj} alt="Kjs picture" />
                     </div>
                 </div>
