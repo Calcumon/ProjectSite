4c3377e5cb2e8d8ced4692c76f056a1d3eb387db
erica2605@live.com <erica2605@live.com>
2019-11-21 17:21:11 -0800

    ericas first commit

--- client/src/components/Container/index.tsx
+++ client/src/components/Container/index.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import MobileNavbar from './components/MobileNavbar';
+
+
 
 interface ContainerInterface {
     currentPage: string,
     bgColor: string,
 }
 
-const Container : React.FC<ContainerInterface> = ({ currentPage, bgColor, children }) => {
-    return (
-        <div className={ bgColor }>
-            <Navbar selected={ currentPage }/>
+interface IState {
+    width: number,
+    height: number,
+    mobileMenuIsOpen: boolean,
+  }
+  
+
+class Container extends React.Component<ContainerInterface, IState> {
+
+    constructor(props:ContainerInterface) {
+        super(props);
+        this.state = { 
+            width: 0, 
+            height: 0,
+            mobileMenuIsOpen: false,
+         };
+
+         
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+      
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+      
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+      
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
+    render() {
+        
+        const { currentPage, bgColor, children } = this.props
+        return (<div className={ bgColor }>
+            { (this.state.width < 950) ? (<MobileNavbar/>) : (<Navbar selected={ currentPage }/>)}
+            
             { children }
             <Footer />
         </div>
-    )
+        )
+    }
 }
 
 export default Container;
