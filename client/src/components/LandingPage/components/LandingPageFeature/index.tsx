ad2cfb56651bac982c7d7fd9a45da887aae45a56
Anna Pawl <annampawl@gmail.com>
2020-02-12 14:31:35 -0800

    replaced feature images and updating text

--- client/src/components/LandingPage/components/LandingPageFeature/index.tsx
+++ client/src/components/LandingPage/components/LandingPageFeature/index.tsx
@@ -8,23 +8,23 @@ export interface LandingPageFeatureProps {
     i: number;
 }
 
-const LandingPageFeature : React.FC<LandingPageFeatureProps> = ({ title, description, image, i }) => {
-    if (i%2 === 0) {
+const LandingPageFeature: React.FC<LandingPageFeatureProps> = ({ title, description, image, i }) => {
+    if (i % 2 === 0) {
         return <div className="feature">
             <div className="featureText">
-                <h2>{ title }</h2>
-                <p>{ description }</p>
+                <h2>{title}</h2>
+                <p>{description}</p>
             </div>
-            <img src={ image } alt={ title }/>
+            <img src={image} alt={title} />
         </div>
     }
-    
+
     return (
         <div className="feature">
-            <img src={ image } alt={ title }/>
+            <img src={image} alt={title} />
             <div className="featureText">
-                <h2>{ title }</h2>
-                <p>{ description }</p>
+                <h2>{title}</h2>
+                <p>{description}</p>
             </div>
         </div>
     )
