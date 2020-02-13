f7a90b822ab70bbc810efc9d31135c4c06b450a4
Anisha Jain <anishajain@outlook.com>
2020-02-12 16:01:20 -0800

    Landing page tests fixed

--- client/src/components/Container/tests/index.spec.tsx
+++ client/src/components/Container/tests/index.spec.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { mount, configure } from 'enzyme';
-configure({adapter: new Adapter()});
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { mount, configure } from "enzyme";
+configure({ adapter: new Adapter() });
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import Container from '../index';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Container from "../index";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-describe('<Container />', () => {
-  function createComponent(page:string, color:string) {
+describe("<Container />", () => {
+  function createComponent(page: string, color: string) {
     return mount(
-      <BrowserRouter><Container currentPage={page} bgColor={color}>
+      <BrowserRouter>
+        <Container currentPage={page} bgColor={color}>
           <p>Hello</p>
-      </Container></BrowserRouter>)
+        </Container>
+      </BrowserRouter>
+    );
   }
-  it('allows us to set props', () => {
-    const wrapper = createComponent("landing", "green") 
-    expect(wrapper.find(Container).props().currentPage).toEqual('landing');
-    wrapper.setProps({ currentPage: 'blue' });
-    expect(wrapper.props().currentPage).toEqual('blue');
+  it("allows us to set props", () => {
+    const wrapper = createComponent("landing", "green");
+    expect(wrapper.find(Container).props().currentPage).toEqual("landing");
+    wrapper.setProps({ currentPage: "blue" });
+    expect(wrapper.props().currentPage).toEqual("blue");
   });
 
-  it('renders navbar and footer', () => {
-    const wrapper = createComponent("landing", "green") 
+  it("renders navbar and footer", () => {
+    const wrapper = createComponent("landing", "green");
     expect(wrapper.exists(Navbar)).toEqual(true);
     expect(wrapper.exists(Footer)).toEqual(true);
   });
-
 });
--- client/src/components/LandingPage/tests/index.spec.tsx
+++ client/src/components/LandingPage/tests/index.spec.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { mount, configure } from 'enzyme';
-configure({adapter: new Adapter()});
-import LandingPage from '..';
-import LandingPageFeature from '../components/LandingPageFeature';
-import Container from '../../Container';
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { mount, configure } from "enzyme";
+configure({ adapter: new Adapter() });
 
-describe('<LandingPage />', () => {
-    it('has no props', () => {
-      const wrapper = mount(
-          <LandingPage />);
-      expect(wrapper.props()).toEqual({});
-    });
-  
-    it('renders', () => {
-      const wrapper = mount(
-          <LandingPage />);
-      expect(wrapper.find(LandingPageFeature)).toHaveLength(4);
-      expect(wrapper.find(Container)).toHaveLength(1);
-      expect(wrapper.find('img')).toHaveLength(6); // 2 in container, 1 for each Feature
-      expect(wrapper.find('h1')).toHaveLength(2);
-    });
-  
+import { BrowserRouter } from "react-router-dom";
+
+import LandingPage from "..";
+import LandingPageFeature from "../components/LandingPageFeature";
+import Container from "../../Container";
+
+describe("<LandingPage />", () => {
+  function createComponent() {
+    return mount(
+      <BrowserRouter>
+        <LandingPage />
+      </BrowserRouter>
+    );
+  }
+  it("has no props", () => {
+    const wrapper = createComponent();
+    expect(wrapper.props()).toEqual({});
   });
+
+  it("renders", () => {
+    const wrapper = createComponent();
+    expect(wrapper.find(LandingPageFeature)).toHaveLength(4);
+    expect(wrapper.find(Container)).toHaveLength(1);
+    expect(wrapper.find("img")).toHaveLength(6); // 2 in container, 1 for each Feature
+    expect(wrapper.find("h1")).toHaveLength(2);
+  });
+});
