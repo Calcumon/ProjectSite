6a9b3a01b603760f0036dc44632a333f2dca4da8
Anna Pawl <annampawl@gmail.com>
2020-02-19 12:02:49 -0800

    getting ready for merge

--- client/src/components/AboutUs/components/Banner/index.tsx
+++ client/src/components/AboutUs/components/Banner/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './styles.scss';
-import BannerWrapper from '../../../BannerWrapper'
-import cutie from '../../../../assets/cuties/cutie3.png'
+import React from "react";
+import "./styles.scss";
+import BannerWrapper from "../../../BannerWrapper";
+import cutie from "../../../../assets/cuties/cutie3.png";
 
-const Banner : React.FC = () => {
-    return (
-        <BannerWrapper image={cutie}>
-            <h1 className="mission">Our mission is to creatively reshape the learning process.</h1>
-        </BannerWrapper>
-    )
-}
+const Banner: React.FC = () => {
+  return (
+    <BannerWrapper image={cutie}>
+      <h1 className="mission">
+        Our mission is to creatively reshape the learning process.
+      </h1>
+    </BannerWrapper>
+  );
+};
 
 export default Banner;
